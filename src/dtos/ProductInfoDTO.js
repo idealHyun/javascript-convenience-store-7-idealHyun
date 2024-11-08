ebddb29a83cfda0e818ed61fdbb4c9a47e479879
SanghyunLee <lgw9736@naver.com>
2024-11-08 23:13:58 +0900

    feat: ProductInfoDTO 클래스 생성
    - 특정 이름의 물건에 대한 정보를 가지는 DTO
    - 정적 팩토리 메소드 사용

---
+++ src/dtos/ProductInfoDTO.js
@@ -0,0 +1,47 @@
+class ProductInfoDTO {
+  #productName;
+  #price;
+  #productQuantity;
+  #promotionProductQuantity;
+  #promotionName;
+
+  constructor(productName, price, productQuantity, promotionProductQuantity, promotionName) {
+    this.#productName = productName;
+    this.#price = price;
+    this.#productQuantity = productQuantity;
+    this.#promotionProductQuantity = promotionProductQuantity;
+    this.#promotionName = promotionName;
+  }
+
+  static of(productInfo) {
+    return new ProductInfoDTO(
+      productInfo.productName,
+      productInfo.price,
+      productInfo.productQuantity,
+      productInfo.promotionProductQuantity,
+      productInfo.promotionName,
+    );
+  }
+
+  getProductName() {
+    return this.#productName;
+  }
+
+  getPrice() {
+    return this.#price;
+  }
+
+  getProductQuantity() {
+    return this.#productQuantity;
+  }
+
+  getPromotionProductQuantity() {
+    return this.#promotionProductQuantity;
+  }
+
+  getPromotionName() {
+    return this.#promotionName;
+  }
+}
+
+export default ProductInfoDTO;
