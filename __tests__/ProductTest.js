659511033f449e8531c7d92e3fe19dffda4ffaa8
SanghyunLee <lgw9736@naver.com>
2024-11-08 20:02:24 +0900

    test: Product 클래스 테스트 코드
    - 물건 이름과 가격은 필수 값

---
+++ __tests__/ProductTest.js
@@ -0,0 +1,36 @@
+describe('Product 테스트', () => {
+  test('Product 생성자 테스트', () => {
+    // given
+    const name = '젤리';
+    const price = '1500';
+
+    // when
+    const product = new Product(name, price);
+
+    // then
+    expect(product.getName())
+      .toBe('젤리');
+    expect(product.getPrice())
+      .toBe(1500);
+  });
+
+  test('Product 이름 없는 예외 테스트', () => {
+    const name = null;
+    const price = '1500';
+
+    expect(() => {
+      new Product(name, price);
+    })
+      .toThrow('[ERROR]');
+  });
+
+  test('Product 가격 없는 예외 테스트', () => {
+    const name = '젤리';
+    const price = null;
+
+    expect(() => {
+      new Product(name, price);
+    })
+      .toThrow('[ERROR]');
+  });
+});
