1c5b78618cd85f6c364ee2ee77af2ec222cad667
SanghyunLee <lgw9736@naver.com>
2024-11-08 20:19:24 +0900

    test: PromotionProductStock 테스트 작성
    - ProductStock 을 상속받을 예정
    - 프로모션이 있는 물건에 대한 개수 상태
    - 물건의 이름과 수량, 프로모션 이름은 필수 값
    - 물건의 수량은 최소 개수보다 커야함

---
+++ __tests__/PromotionProductStockTest.js
@@ -0,0 +1,33 @@
+describe('PromotionProductStock 테스트', ()=>{
+  test('생성자 테스트',()=>{
+    const productName = '콜라'
+    const quantity = '10';
+    const promotionName = '탄산2+1'
+
+    expect(()=>{new PromotionProductStock(productName,quantity,promotionName)}).not.toThrow();
+  })
+
+  test('프로모션 없는 생성자 테스트',()=>{
+    const productName = '콜라'
+    const quantity = '10';
+    const promotionName = null
+
+    expect(()=>{new PromotionProductStock(productName,quantity,promotionName)}).toThrow('[ERROR]');
+  })
+
+  test('productName 없는 생성자 예외 테스트',()=>{
+    const productName = null
+    const quantity = '10';
+    const promotionName = '탄산2+1'
+
+    expect(()=>{new PromotionProductStock(productName,quantity,promotionName)}).toThrow('[ERROR]');
+  })
+
+  test('quantity 없는 생성자 예외 테스트',()=>{
+    const productName = '콜라'
+    const quantity = null
+    const promotionName = '탄산2+1'
+
+    expect(()=>{new PromotionProductStock(productName,quantity,promotionName)}).toThrow('[ERROR]');
+  })
+})
