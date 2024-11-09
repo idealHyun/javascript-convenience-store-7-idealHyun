bb23d829eceac624358818f4bb5079de3391ca11
SanghyunLee <lgw9736@naver.com>
2024-11-09 18:38:17 +0900

    feat: Receipt 클래스 생성
    - 결제한 물건 정보와 증정된 물건 정보에 대해 소유하고 보여줄 예정

---
+++ src/models/Receipt.js
@@ -0,0 +1,19 @@
+class Receipt {
+  #purchasedProducts
+  #bonusProducts
+
+  constructor() {
+    this.#purchasedProducts = []
+    this.#bonusProducts = []
+  }
+
+  addPurchasedProductStock(purchasedProductStock) {
+
+  }
+
+  addBonusProductStock(purchasedProductStock) {
+
+  }
+}
+
+export default Receipt;
