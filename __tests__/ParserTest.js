c597b2fe79ca2491e3c4ba7088ae116f863b7bce
SanghyunLee <lgw9736@naver.com>
2024-11-09 16:17:43 +0900

    fix: ParserTest 수정
    - Map 형태로 바꾼 뒤 코드 수정

--- __tests__/ParserTest.js
+++ __tests__/ParserTest.js
@@ -1,6 +1,7 @@
 import Parser from '../src/models/Parser.js';
 import DocsLoader from '../src/models/DocsLoader.js';
 import { DOCS_CONFIG } from '../src/constants/docsConfig.js';
+import Promotion from '../src/models/Promotion.js';
 
 describe('Parser 테스트', () => {
   test('파싱', async () => {
@@ -10,28 +11,21 @@ describe('Parser 테스트', () => {
     const parser = new Parser();
     const parsedData = parser.parsePromotionsData(promotionData);
 
-    expect(parsedData).toEqual({
-      '탄산2+1': {
-        name: '탄산2+1',
-        buy: 2,
-        get: 1,
-        startDate: '2024-01-01',
-        endDate: '2024-12-31',
-      },
-      MD추천상품: {
-        name: 'MD추천상품',
-        buy: 1,
-        get: 1,
-        startDate: '2024-01-01',
-        endDate: '2024-12-31',
-      },
-      반짝할인: {
-        name: '반짝할인',
-        buy: 1,
-        get: 1,
-        startDate: '2024-11-01',
-        endDate: '2024-11-30',
-      },
-    });
+    const expectedData = new Map([
+      [
+        '탄산2+1',
+        new Promotion('탄산2+1', 2, 1, '2024-01-01', '2024-12-31')
+      ],
+      [
+        'MD추천상품',
+        new Promotion('MD추천상품', 1, 1, '2024-01-01', '2024-12-31')
+      ],
+      [
+        '반짝할인',
+        new Promotion('반짝할인', 1, 1, '2024-11-01', '2024-11-30')
+      ]
+    ]);
+
+    expect(parsedData).toEqual(expectedData);
   });
 });
