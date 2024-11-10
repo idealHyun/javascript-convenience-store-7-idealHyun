3be96d9e680f4cb32cb2919d6a1acedbcd72dfb6
SanghyunLee <lgw9736@naver.com>
2024-11-11 01:35:01 +0900

    refactor: 출력문 리팩토링
    - 변수들의 상수화

--- src/constants/storeConfig.js
+++ src/constants/storeConfig.js
@@ -5,6 +5,13 @@ const STORE_CONFIG = Object.freeze({
     minimumProductQuantity: 1,
     maximumMembershipDiscount : 8000,
     membershipDiscountRate : 0.3,
+    outOfStock : '재고 없음',
+    koreanMoneyUnit : '원',
+    productUnit : '개',
+    totalQuantityWord : '총구매액',
+    promotionalDiscountWord : '행사할인',
+    memberShipDiscountWord : '멤버십할인',
+    amountToPayWord : '내실돈'
   }
 );
 
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -3,6 +3,12 @@ import { SYSTEM_MESSAGE } from '../constants/message.js';
 import { STORE_CONFIG } from '../constants/storeConfig.js';
 
 class OutputView {
+  #TAB = '\t';
+  #TWO_TAB = '\t\t';
+  #THREE_TAB = '\t\t\t';
+  #FOUR_TAB = '\t\t\t\t';
+  #THREE = 3;
+
   printMessage(message) {
     Console.print(message);
   }
@@ -12,21 +18,38 @@ class OutputView {
   }
 
   printProductInfo(productInfoDTO) {
-    this.#printPromotionProductInfo(productInfoDTO.getProductName(), productInfoDTO.getPrice(),
-      productInfoDTO.getPromotionProductQuantity(), productInfoDTO.getPromotionName());
-    this.#printProductInfo(productInfoDTO.getProductName(), productInfoDTO.getPrice(),
-      productInfoDTO.getProductQuantity());
+    this.#printPromotionProductInfo(
+      productInfoDTO.getProductName(),
+      productInfoDTO.getPrice(),
+      productInfoDTO.getPromotionProductQuantity(),
+      productInfoDTO.getPromotionName(),
+    );
+    this.#printProductInfo(
+      productInfoDTO.getProductName(),
+      productInfoDTO.getPrice(),
+      productInfoDTO.getProductQuantity(),
+    );
   }
 
-  printExceedPromotionProductInfo(productPromotionInfoDTO){
-    Console.print(SYSTEM_MESSAGE.output.promotionExceedQuantity(productPromotionInfoDTO.getProductName(),productPromotionInfoDTO.getProductQuantity()));
+  printExceedPromotionProductInfo(productPromotionInfoDTO) {
+    Console.print(
+      SYSTEM_MESSAGE.output.promotionExceedQuantity(
+        productPromotionInfoDTO.getProductName(),
+        productPromotionInfoDTO.getProductQuantity(),
+      ),
+    );
   }
 
-  printGuidePromotionProductInfo(productPromotionInfoDTO){
-    Console.print(SYSTEM_MESSAGE.output.promotionGuide(productPromotionInfoDTO.getProductName(),productPromotionInfoDTO.getProductQuantity()));
+  printGuidePromotionProductInfo(productPromotionInfoDTO) {
+    Console.print(
+      SYSTEM_MESSAGE.output.promotionGuide(
+        productPromotionInfoDTO.getProductName(),
+        productPromotionInfoDTO.getProductQuantity(),
+      ),
+    );
   }
 
-  printReceipt(receiptDTO){
+  printReceipt(receiptDTO) {
     Console.print(SYSTEM_MESSAGE.output.initialReceipt);
     Console.print(SYSTEM_MESSAGE.output.purchasedProductsLine);
     this.#printPurchasedProducts(receiptDTO.getPurchasedProducts());
@@ -38,42 +61,66 @@ class OutputView {
 
   #printPurchasedProducts(purchasedProductDTOs) {
     purchasedProductDTOs.forEach((purchasedProductDTO) => {
+      const productName = purchasedProductDTO.getName();
+      let tabSize =
+        productName.length > this.#THREE ? this.#TWO_TAB : this.#THREE_TAB;
       Console.print(
-        `${purchasedProductDTO.getName()}\t\t\t${purchasedProductDTO.getQuantity()}\t${purchasedProductDTO.getAmount().toLocaleString()}`
+        `${productName} ${tabSize} ${purchasedProductDTO.getQuantity()} ${this.#TAB} ${purchasedProductDTO.getAmount().toLocaleString()}`,
       );
     });
   }
 
   #printBonusProducts(bonusProductDTOs) {
     bonusProductDTOs.forEach((bonusProductDTO) => {
-      Console.print(`${bonusProductDTO.getName()}\t\t\t${bonusProductDTO.getQuantity()}`);
+      const productName = bonusProductDTO.getName();
+      let tabSize =
+        productName.length > this.#THREE ? this.#TWO_TAB : this.#THREE_TAB;
+      Console.print(
+        `${productName} ${tabSize} ${bonusProductDTO.getQuantity()}`,
+      );
     });
   }
 
   #printTotalInfo(totalInfoDTO) {
-    Console.print(`총구매액\t\t${totalInfoDTO.getTotalQuantity()}\t${totalInfoDTO.getTotalAmount().toLocaleString()}`);
-    Console.print(`행사할인\t\t\t-${totalInfoDTO.getPromotionDiscount().toLocaleString()}`);
-    Console.print(`멤버십할인\t\t\t-${totalInfoDTO.getMembershipDiscount().toLocaleString()}`);
-    Console.print(`내실돈\t\t\t\t${totalInfoDTO.getFinalAmount().toLocaleString()}`);
+    Console.print(
+      `${STORE_CONFIG.totalQuantityWord} ${this.#TWO_TAB} ${totalInfoDTO.getTotalQuantity()} ${this.#TAB} ${totalInfoDTO.getTotalAmount().toLocaleString()}`,
+    );
+    Console.print(
+      `${STORE_CONFIG.promotionalDiscountWord}${this.#THREE_TAB}-${totalInfoDTO.getPromotionDiscount().toLocaleString()}`,
+    );
+    Console.print(
+      `${STORE_CONFIG.memberShipDiscountWord}${this.#THREE_TAB}-${totalInfoDTO.getMembershipDiscount().toLocaleString()}`,
+    );
+    Console.print(
+      `${STORE_CONFIG.amountToPayWord}${this.#FOUR_TAB}${totalInfoDTO.getFinalAmount().toLocaleString()}`,
+    );
   }
 
-  #printPromotionProductInfo(productName,price,quantity,promotionName) {
-    if (quantity === null ) {
-      return ;
+  #printPromotionProductInfo(productName, price, quantity, promotionName) {
+    if (quantity === null || promotionName === null) {
+      return;
     }
-    if (quantity === 0 ) {
-      Console.print(`- ${productName} ${price.toLocaleString()}원 재고 없음 ${promotionName}`);
-    } else{
-      Console.print(`- ${productName} ${price.toLocaleString()}원 ${quantity}개 ${promotionName}`);
+    if (quantity === 0) {
+      Console.print(
+        `- ${productName} ${price.toLocaleString()}${STORE_CONFIG.koreanMoneyUnit} ${STORE_CONFIG.outOfStock} ${promotionName}`,
+      );
+    } else {
+      Console.print(
+        `- ${productName} ${price.toLocaleString()}${STORE_CONFIG.koreanMoneyUnit} ${quantity}${STORE_CONFIG.productUnit} ${promotionName}`,
+      );
     }
   }
 
-  #printProductInfo(productName,price,quantity) {
-    if (quantity < STORE_CONFIG.minimumProductQuantity || quantity === null ) {
-      Console.print(`- ${productName} ${price.toLocaleString()}원 재고 없음`);
-      return ;
+  #printProductInfo(productName, price, quantity) {
+    if (quantity < STORE_CONFIG.minimumProductQuantity || quantity === null) {
+      Console.print(
+        `- ${productName} ${price.toLocaleString()}${STORE_CONFIG.koreanMoneyUnit} ${STORE_CONFIG.outOfStock}`,
+      );
+      return;
     }
-    Console.print(`- ${productName} ${price.toLocaleString()}원 ${quantity}개`);
+    Console.print(
+      `- ${productName} ${price.toLocaleString()}${STORE_CONFIG.koreanMoneyUnit} ${quantity}${STORE_CONFIG.productUnit}`,
+    );
   }
 }
 
