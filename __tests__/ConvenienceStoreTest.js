c7bfe654d8f89a3e5d2f3ea27f0c5c1f83ceb883
SanghyunLee <lgw9736@naver.com>
2024-11-11 00:04:11 +0900

    test: 상품 입력 예외 테스트
    - 수량 초과 및 음수, 소수 등에 대한 유효성 검증
    - 없는 제품명 검사

--- __tests__/ConvenienceStoreTest.js
+++ __tests__/ConvenienceStoreTest.js
@@ -1,5 +1,40 @@
 import ConvenienceStore from '../src/models/ConvenienceStore.js';
 
+describe('구매할 상품 입력 예외 테스트', () => {
+  let convenienceStore;
+
+  beforeEach(async () => {
+    // given
+    convenienceStore = new ConvenienceStore();
+    await convenienceStore.loadInitData();
+  });
+
+  test.each([
+    {
+      description: '형식이 잘못된 경우 - 수량이 음수',
+      inputString: '[콜라--2]'
+    },
+    {
+      description: '형식이 잘못된 경우 - 수량이 소수',
+      inputString: '[콜라-0.4]'
+    },
+    {
+      description: '형식이 잘못된 경우 - 수량이 0',
+      inputString: '[콜라-0]'
+    },
+    {
+      description: '없는 제품 적기',
+      inputString: '[샌드위치-2]'
+    },
+    {
+      description: '수량 초과하게 적기',
+      inputString: '[콜라-200]'
+    },
+  ])('$description', ({ inputString }) => {
+    expect(() => convenienceStore.getProductStocksToSell(inputString)).toThrow('[ERROR]');
+  });
+});
+
 describe("ConvenienceStore 재고 감소 테스트", () => {
   let convenienceStore;
 
